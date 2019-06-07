4fd7a5b6ec0ee8c9f0001896d2d2152d55e2db09
Daniel Corcoran <danielc.92@outlook.com>
2019-06-07 22:22:15 +1000

    Created lyrics component

---
+++ src/components/Lyrics.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Lyrics extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
