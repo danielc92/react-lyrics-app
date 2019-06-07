5f7251f5e0ca45cdefa4faaeac2604fc4a5a673e
Daniel Corcoran <danielc.92@outlook.com>
2019-06-08 09:22:50 +1000

    Created Search Component

---
+++ src/components/Search.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import axios from 'axios';
+
+export default class  extends Component {
+
+    state = {
+        trackTitle: ''
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
