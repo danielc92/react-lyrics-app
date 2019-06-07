7997aa28edb50283ac5ccc717181567646a3b6f1
Daniel Corcoran <danielc.92@outlook.com>
2019-06-07 13:13:14 +1000

    Added dummy data for Context state

--- src/Context.js
+++ src/Context.js
@@ -2,12 +2,24 @@ import React, { Component } from 'react'
 
 const Context = React.createContext();
 
-export default class Provider extends Component {
+class Provider extends Component {
+
+    state = {
+        track_list = [
+            {track:{track_name:'lorem'}},
+            {track:{track_name:'lorem3'}},
+            {track:{track_name:'lorem2'}}
+        ],
+        heading: 'Top 10 tracks'
+    }
+
     render() {
         return (
-            <Context.Provider>
+            <Context.Provider value={this.state}>
                 {this.props.children}
             </Context.Provider>
         )
     }
 }
+
+export const Consumer = Context.Consumer;
