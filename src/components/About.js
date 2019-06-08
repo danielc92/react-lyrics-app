e1f7d0e0e8ffe8310e51ffd513b034aa516bb07a
Daniel Corcoran <danielc.92@outlook.com>
2019-06-08 11:49:43 +1000

    Filled in About

--- src/components/About.js
+++ src/components/About.js
@@ -7,10 +7,15 @@ export default class About extends Component {
                 <section className="container">
                     <section className="content">
                         <h3>About</h3>
-                        <p>Voluptate adipisicing eiusmod Lorem exercitation. Veniam veniam consequat nisi excepteur anim sint incididunt labore Lorem cillum eiusmod. Et ad cillum proident irure nostrud pariatur sit. Labore sunt exercitation qui occaecat in incididunt.</p>
-                        <p>Voluptate adipisicing eiusmod Lorem exercitation. Veniam veniam consequat nisi excepteur anim sint incididunt labore Lorem cillum eiusmod. Et ad cillum proident irure nostrud pariatur sit. Labore sunt exercitation qui occaecat in incididunt.</p>
-                        <p>Voluptate adipisicing eiusmod Lorem exercitation. Veniam veniam consequat nisi excepteur anim sint incididunt labore Lorem cillum eiusmod. Et ad cillum proident irure nostrud pariatur sit. Labore sunt exercitation qui occaecat in incididunt.</p>
-                        <p>Voluptate adipisicing eiusmod Lorem exercitation. Veniam veniam consequat nisi excepteur anim sint incididunt labore Lorem cillum eiusmod. Et ad cillum proident irure nostrud pariatur sit. Labore sunt exercitation qui occaecat in incididunt.</p>
+                        <p>This application was built in order to start using the Context API to gain a better understanding of global context usage in React, as well as usage of third party API integration with React. Building this posed some challenges as I have not done API integration with React or used the Context API before. The application is currently being served by github pages.</p>
+                        <p>I followed <a href="https://www.youtube.com/watch?v=NDEt0KdDbhk">Traverys tutorials</a> closely and made a lot of adjustments through the project, such as using a different layout, CSS framework and theming.</p>
+                        <p>In general the application encompasses the following technical aspects</p>
+                        <ul>
+                            <li>Use of <strong>axios</strong> library to make API calls to musixmatch service</li>
+                            <li>Use of global context using the Context API</li>
+                            <li>Creating and using dispatchers, reducers, Providers and Consumers</li>
+                        </ul>
+                        <p>This application allows the user upon the first request, to view top 10 tracks in the United States. They can also view lyrics and stats relating to songs that show up in the 'Track List'. Users can also search through the musicmatch database via API calls using the search form to locate track titles of interest.</p>
                     </section>
                 </section>
             </section>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,6 +10,7 @@ export default class Footer extends Component {
                    <p>Styled with <a href="https://bulma.io/">Bulma CSS Framework</a></p>
                    <p>Images from <a href="https://pixabay.com/">Pixabay</a></p>
                    <p>Icons from <a href="https://fontawesome.com/">Fontawesome</a></p>
+                    <p>Integrating search functionality in the app by passing url parameters to the API call</p>
                 </div>
             </div>
         </footer>
