197610326e360a5684723acdae08c6311d560d1a
Daniel Corcoran <danielc.92@outlook.com>
2019-06-08 10:51:38 +1000

    Updates

---
+++ precache-manifest.599df19839851ed75f71859cefb3b508.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de7bc72d8474f2cca7e08570e282cc34",
+    "url": "/react-lyrics-app/index.html"
+  },
+  {
+    "revision": "9de0376db89e65324db2",
+    "url": "/react-lyrics-app/static/css/main.8ef63b32.chunk.css"
+  },
+  {
+    "revision": "d6b9ede1b72ddcf158db",
+    "url": "/react-lyrics-app/static/js/2.1dc6ba3a.chunk.js"
+  },
+  {
+    "revision": "9de0376db89e65324db2",
+    "url": "/react-lyrics-app/static/js/main.08b7f054.chunk.js"
+  },
+  {
+    "revision": "73fe495d60094e20816b",
+    "url": "/react-lyrics-app/static/js/runtime~main.765538a3.js"
+  },
+  {
+    "revision": "6d3a9254b02607e9f5af73de3eb52bae",
+    "url": "/react-lyrics-app/static/media/musicians-690591_1920.6d3a9254.jpg"
+  }
+]);
