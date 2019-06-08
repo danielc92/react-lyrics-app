11af21b1f5885e823cabeef981be9c4ef81661ee
Daniel Corcoran <danielc.92@outlook.com>
2019-06-08 17:16:27 +1000

    Updates

--- precache-manifest.117548f563250e6ccd9b936618b15162.js
+++ precache-manifest.117548f563250e6ccd9b936618b15162.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5886a8b0ceabd7b7fdd0fdafbb80c131",
+    "revision": "3f5cdf5243a4a0246442589ec77fa317",
     "url": "/react-lyrics-app/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-lyrics-app/static/js/2.c46056fa.chunk.js"
   },
   {
-    "revision": "c71457a8518ed1725bff",
-    "url": "/react-lyrics-app/static/js/main.c49d181f.chunk.js"
+    "revision": "8af840b1c10935a21673",
+    "url": "/react-lyrics-app/static/js/main.e8e12d52.chunk.js"
   },
   {
     "revision": "73fe495d60094e20816b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-lyrics-app/precache-manifest.ef46e55c7f65bd1a11b022c27ef025cd.js"
+  "/react-lyrics-app/precache-manifest.117548f563250e6ccd9b936618b15162.js"
 );
 
 self.addEventListener('message', (event) => {
